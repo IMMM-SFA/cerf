90dd7dd49297d638818cab5ae68befa6457ffd25
Vernon, Chris R <chris.vernon@pnnl.gov>
2018-01-05 17:19:21 -0500

    update reqs and add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    sys.stdout.write('Missing Package:  setuptools not found.  Demeter requires this to install.  Please install setuptools and retry.')
+    sys.exit(1)
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
+
+
+setup(
+    name='cerf',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/IMMM-SFA/cerf',
+    license='BSD 2-Clause',
+    author='Chris R. Vernon; Nino Zuljevic',
+    author_email='chris.vernon@pnnl.gov; nino.zuljevic@pnnl.gov',
+    description='A Geospatial Model for Assessing Future Technology Expansion Feasibility',
+    long_description=readme(),
+    install_requires=get_requirements()
+)
