e8e44395e3299fb1beb5e52eef32862f411c2f1e
crvernon <chris.vernon@pnnl.gov>
2020-01-28 11:18:02 -0500

    remove cpp site module

--- site/core/siting_model.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:57d19a15c5f2b2766c35804e36b0863beccd082013c394fe50bca80e36d27aaf
-size 50385
--- site/core/tinyxml2.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6a2596f65db6c0c3474e8e087d71c9f90b208f95d27dbd937bfd07defa6b6db7
-size 69317
