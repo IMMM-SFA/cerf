e4b0fee75c10bc09389ded0ab2d83cfe4d7b3e75
crvernon <chris.vernon@pnnl.gov>
2020-01-28 22:01:46 -0500

    building equations

--- cerf/nov.py
+++ cerf/nov.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 class Nov:
     """Calculate Net Operating Value (NOV) in ($ / yr) as the following:
 
@@ -27,28 +24,74 @@ class Nov:
                     where, d = real annual discount rate (%)
                             n = asset lifetime (years)```
 
+    :param technology_dict:         Dictionary of technology-specific data. E.g., {techid: unit_size: 600
+
     """
 
-    def __init__(self):
+    def __init__(self, technology_dict, discount_rate, lmp_array):
 
-        pass
+        self.tech_dict = technology_dict
+        self.discount_rate = discount_rate
+        self.lmp_arr = lmp_array
+
+        self.arr = np.zeros_like(self.lmp_arr)  # techid, col, row
 
     def calc_levelization_factor(self):
         """Calculate levelization factor"""
 
-        pass
+        annuity_factor, lifetime_arr = self.calc_annuity_factor()
+
+        # TODO:  the value for l, real annual growth rate is missing (%)
+        l = 0.1
+
+        k = (1 + l) / (1 + self.discount_rate)
+
+        return k * (1 - k ** lifetime_arr) * (annuity_factor / (1 - k))
 
     def calc_annuity_factor(self):
-        """Calculate annuity factor"""
+        """Calculate annuity factor as (d(1 + d)**n) / ((1 + d)**n - 1)
+            where, d = real annual discount rate (%), n = asset lifetime (years)
 
-        pass
+        """
+        lifetime_arr = self.arr.copy()
+
+        for i in range(0, len(d)):
+            lifetime_arr[i] = lifetime_arr[i] + self.tech_dict[i + 1]["lifetime"]
+
+        numer = self.discount_rate * (1 + self.discount_rate) ** lifetime_arr
+        denom = (1 + self.discount_rate) ** lifetime_arr - 1
+
+        return numer / denom, lifetime_arr
 
     def calc_operating_costs(self):
         """Calculate operating costs"""
 
-        pass
+        # TODO: carbon price ($ / ton) not accounted for
+        carbon_price = 0
+
+        # TODO: carbon fuel content (tons / Btu) not accounted for
+        carbon_fuel_content = 0
+
+        # TODO:  why does heat_rate apprear twice?
+        operating_costs = [d[i]["heat_rate"] * d[i]["fuel_price"] + d[i]["variable_om"] + carbon_price *
+                           carbon_fuel_content * d[i]["heat_rate"] * (1 - d[i]["carbon_capture_rate"])
+                           for i in range(1, len(d) + 1)]
+
+        arr = self.arr.copy()
+
+        for index, i in enumerate(operating_costs):
+            arr[index] += i
+
+        return arr
 
     def calc_nov(self):
         """Calculate NOV"""
 
-        pass
+        #         Generation (MWh / yr) *
+        #                         [ Locational Marginal Price ($ / MWh) - Operating Costs ($ / MWh) ] *
+        #                         Levelization Factor
+
+        lev_factor = self.calc_levelization_factor()
+        op_costs = self.calc_operating_costs()
+
+        # TODO: start here
