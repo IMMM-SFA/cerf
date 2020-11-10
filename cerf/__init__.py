3e7a6039befe70bc4cd3368e02bc0eb44aa2be19
crvernon <chrisrvernon@gmail.com>
2020-11-10 17:35:14 -0500

    raise to package level

--- cerf/__init__.py
+++ cerf/__init__.py
@@ -1,4 +1,5 @@
 from cerf.model import *
 from cerf.install_supplement import *
+from cerf.nov import NetOperationalValue
 
-__all__ = ['model', 'InstallSupplement']
+__all__ = ['model', 'InstallSupplement', 'NetOperationalValue']
