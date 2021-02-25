5b60b58de874ceeceb0a48bffb9e84bb114dc92c
crvernon <chrisrvernon@gmail.com>
2021-02-25 10:37:51 -0500

    update config reader protocol to yaml

--- cerf/read_config.py
+++ cerf/read_config.py
@@ -1,52 +1,42 @@
-"""Configuration reader for CERF
+import os
+import logging
 
-@author Chris R. vernon
-@email chris.vernon@pnnl.gov
-
-License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
-
-"""
 import yaml
 
 
 class ReadConfig:
-    """Read configuration data either provided in the configuration YAML file or as passed in via arguments.
-    :param config_file:                         string. Full path to configuration YAML file with file name and
-                                                extension. If not provided by the user, the code will default to the
-                                                expectation of alternate arguments.
 
-    """
+    def __init__(self, **kwargs):
 
-    def __init__(self, config_file=None, biomass_directory=None, nuclear_directory=None):
+        # yaml config file
+        self.config_file = kwargs.get('config_file', None)
 
-        if config_file is None:
+        # read into dict
+        self.config = self.get_yaml()
 
-            self.biomass_directory = biomass_directory
-            self.nuclear_directory = nuclear_directory
+    def get_yaml(self):
+        """Read the YAML config file.
+
+        :param config_file:                 Full path with file name and extension to the input config.yml file
+        :type config_file:                  str
+
+        :return:                    YAML config object
+
+        """
+
+        # if config file not passed
+        if self.config_file is None:
+            msg = "Config file must be passed as an argument using:  config_file='<path to config.yml'>"
+            logging.info(msg)
+            raise AttributeError(msg)
+
+        # check for path exists
+        if os.path.isfile(self.config_file):
+
+            with open(self.config_file, 'r') as yml:
+                return yaml.load(yml, Loader=yaml.FullLoader)
 
         else:
-
-            # extract config file to YAML object
-            cfg = self.get_yaml(config_file)
-
-            cfg_suitability = self.validate_key(cfg, 'suitability')
-
-            self.biomass_directory = cfg_suitability['biomass_directory']
-            self.nuclear_directory = cfg_suitability['nuclear_directory']
-
-    @staticmethod
-    def validate_key(yaml_object, key):
-        """Check to see if key is in YAML file, if not return None"""
-        try:
-            return yaml_object[key]
-        except KeyError:
-            return None
-
-    @staticmethod
-    def get_yaml(config_file):
-        """Read the YAML config file
-        :param config_file:         Full path with file name and extension to the input config.yml file
-        :return:                    YAML config object
-        """
-        with open(config_file, 'r') as yml:
-            return yaml.load(yml)
+            msg = f"Config file not found for path:  {self.config_file}"
+            logging.info(msg)
+            raise FileNotFoundError(msg)
