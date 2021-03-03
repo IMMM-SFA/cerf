b8778e6c03c344b60eca914db2dc8562f893e281
crvernon <chrisrvernon@gmail.com>
2021-03-03 11:59:11 -0500

    set up module list

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,10 @@
 #
 import os
 import sys
+
+# path to cerf python package
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../'))
+
 
 # -- Project information -----------------------------------------------------
 
@@ -28,18 +29,10 @@ release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
-# Adding this line to avoid error:
-# Sphinx error:contents.rst not found
-# From: https://github.com/readthedocs/readthedocs.org/issues/2569
-master_doc = 'index'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon','sphinx.ext.autosummary']
-
-autosummary_generate = True
-
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,24 +40,19 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# html_theme = 'alabaster'
+
+import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
-numfig = True
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# Overriding CSS
-html_css_files = [
-    'css/custom.css',
-]
--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 
 def get_requirements():
     with open('requirements.txt') as f:
@@ -20,7 +16,6 @@ setup(
     author='Chris R. Vernon; Nino Zuljevic',
     author_email='chris.vernon@pnnl.gov; nino.zuljevic@pnnl.gov',
     description='A Geospatial Model for Assessing Future Energy Technology Expansion Feasibility',
-    long_description=readme(),
     install_requires=get_requirements(),
     python_requires='>=3.6',
     include_package_data=True
