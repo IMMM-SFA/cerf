1299052c097b67b2729f1a0b23c2b5a21dcf8acf
crvernon <chrisrvernon@gmail.com>
2021-05-06 16:50:29 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def readme():
+    """Return the contents of the project README file."""
+    with open('README.md') as f:
+        return f.read()
 
 
 def get_requirements():
@@ -8,5 +14,17 @@ def get_requirements():
 
 
 setup(
+    name='cerf',
+    version='2.0.0',
+    packages=find_packages(),
+    url='https://github.com/IMMM-SFA/cerf',
+    license='BSD2-Simplified',
+    author='Chris R. Vernon; Nino Zuljevic',
+    author_email='chris.vernon@pnnl.gov; nino.zuljevic@pnnl.gov',
+    description='A Geospatial Model for Assessing Future Energy Technology Expansion Feasibility',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7.*, <4',
+    include_package_data=True,
     install_requires=get_requirements()
 )
