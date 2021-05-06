5699fb0a320a6589ed659e6ffc81fbc8029f5b53
crvernon <chrisrvernon@gmail.com>
2021-05-06 16:22:21 -0400

    change to setup.cfg protocol

--- setup.py
+++ setup.py
@@ -1,27 +1,2 @@
-from setuptools import setup, find_packages
-
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-def get_requirements():
-    with open('requirements.txt') as f:
-        return f.read().split()
-
-
-setup(
-    name='cerf',
-    version='2.0.0',
-    packages=find_packages(),
-    url='https://github.com/IMMM-SFA/cerf',
-    license='BSD 2-Clause',
-    author='Chris R. Vernon; Nino Zuljevic',
-    author_email='chris.vernon@pnnl.gov; nino.zuljevic@pnnl.gov',
-    description='A Geospatial Model for Assessing Future Energy Technology Expansion Feasibility',
-    long_description=readme(),
-    install_requires=get_requirements(),
-    python_requires='>=3.7',
-    include_package_data=True
-)
+from setuptools import setup
+setup()
