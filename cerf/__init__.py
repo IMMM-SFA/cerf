3a7c0d4216bb1257dee983184acff247cbc48e3a
crvernon <chrisrvernon@gmail.com>
2021-06-29 14:40:23 -0400

    raise fn to package level

--- cerf/__init__.py
+++ cerf/__init__.py
@@ -3,7 +3,9 @@ from cerf.install_supplement import *
 from cerf.model import *
 from cerf.outputs import *
 from cerf.parallel import run_parallel
-from cerf.transmission import process_hifld_substations, transmission_to_distance_raster
+from cerf.transmission import *
+
 
 __all__ = ['model', 'InstallSupplement', 'run_parallel', 'config_file', 'plot_results', 'get_package_data',
-           'process_hifld_substations', 'transmission_to_distance_raster', 'cerf_crs']
+           'process_hifld_substations', 'transmission_to_distance_raster', 'cerf_crs',
+           'process_eia_natural_gas_pipelines']
