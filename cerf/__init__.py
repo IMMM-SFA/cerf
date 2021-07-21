b3f54e80d1e00afae7f8e0e86c8f712f539d5712
crvernon <chrisrvernon@gmail.com>
2021-07-21 17:29:15 -0400

    update install packages call in setup for pip

--- cerf/__init__.py
+++ cerf/__init__.py
@@ -7,4 +7,4 @@ from .lmp import *
 from .utils import *
 
 
-__version__ = "2.0.0-beta.1"
+__version__ = "2.0.0-beta.3"
--- setup.py
+++ setup.py
@@ -30,7 +30,23 @@ setup(
     long_description_content_type="text/markdown",
     python_requires='>=3.7.*, <4',
     include_package_data=True,
-    install_requires=get_requirements(),
+    install_requires=[
+        'numpy~=1.19.4',
+        'pandas~=1.1.4',
+        'rasterio~=1.2.3',
+        'xarray~=0.16.1',
+        'PyYAML~=5.4.1',
+        'requests~=2.25.1',
+        'joblib~=1.0.1',
+        'matplotlib~=3.3.3',
+        'seaborn~=0.11.1',
+        'whitebox~=1.5.1',
+        'fiona~=1.8.19',
+        'pyproj~=3.0.1',
+        'rtree~=0.9.7',
+        'shapely~=1.7.1',
+        'geopandas~=0.9.0'
+    ],
     extras_require={
         'dev': [
             'build~=0.5.1',
